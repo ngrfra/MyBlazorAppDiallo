2e24c68efe34bfbd03b268da4ca1f482a6b28db4
ngrdiallo <mdiallo@studenti.apuliadigitalmaker.it>
2025-09-11 12:16:30 +0200

    Descrizione modifiche

--- Program.cs
+++ Program.cs
@@ -9,3 +9,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+/* 
+ * This code sets up a Blazor WebAssembly application.
+ * It configures the root components and registers an HttpClient service
+ * with a base address derived from the host environment.
+ * Finally, it builds and runs the application asynchronously.
+ */
